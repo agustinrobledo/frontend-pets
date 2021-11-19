b562393738a081d1ad4be748632292d7261662c4
agustinrobledo <agustinrobledo59.ar@gmail.com>
2021-11-18 21:12:59 -0300

    added controlled form and styles

--- src/api/index.js
+++ src/api/index.js
@@ -9,7 +9,9 @@ export const getRazas = async () => {
     }
 }
 
-export const postPets = async (data) => {
+export const postPets = async (data, years, months) => {
+    let aprox = false
+    if(years !== 0 || months !== 0) aprox = true
     const response = await fetch('http://localhost:1337/mascotas', {
         method: 'POST',
         body: JSON.stringify({
@@ -17,6 +19,9 @@ export const postPets = async (data) => {
             Raza: data.raza,
             Edad: data.edad,
             Sexo: data.sexo,
+            Año: years,
+            Mes: months,
+            Aproximado: aprox
         }),
         headers: {
             'Content-Type': 'application/json'
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import { useEffect, useState } from "react";
 import '../../scss/form.scss'
 import { getRazas, postPets} from '../../api'
+
+const today = () => {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${year}-${month}-${day}`;
+};
+
+
 const Form = (props) => {
     const [razas, setRazas] = useState([]);
     const [form, setForm] = useState({
@@ -17,14 +27,39 @@ const Form = (props) => {
             [e.target.name]: e.target.value
         })
     }
+    const [years, setYears] = useState(0);
+    const [months, setMonths] = useState(0);
+    const onClickChange = () => {
+        setDisabled(!disabled)
+        if(disabled){
+            setForm({
+                ...form,
+                edad: '',
+            })
+        }
+        else{
+            setMonths(0)
+            setYears(0)
+        }
+    }
+    const [disabledAge, setDisabledAge] = useState(true);
+    const onClickChangeAge = () => {
+        setDisabledAge(!disabledAge)
+        if(disabledAge){
+            setYears(0);
+        }
+        else{
+            setMonths(0);
+        }
+    }
     const formSubmit = (e) => {
         e.preventDefault();
-        postPets(form)
+        postPets(form, years, months)
         .then(res => {
-            console.log(res);
+            alert('Mascota creada con exito')
         })
         .catch(err => {
-            console.log(err);
+            alert('Error al crear mascota')
         })
     }
         
@@ -37,30 +72,78 @@ const Form = (props) => {
 
     return (
         <div className="container">
-            <h1>Form</h1>
+            <h1>Registra a tu mascota</h1>
             <form className="form-container" onSubmit={formSubmit}>
-                <input type="text" name="nombre" placeholder="Name" onChange={formChange} />
-                <input type="date" name="edad" placeholder="Age" onChange={formChange} />
-                <select name="raza" onChange={formChange}>
+                <input type="text" className={!form.nombre && "danger"} name="nombre" placeholder="Name" onChange={formChange} />
+                {disabled && <input type="date" className={!form.edad && "danger"} name="edad" placeholder="Age" onChange={formChange} max={today()} />}
+                {disabled && <h3>¿No sabes cuando nació exactamente tu mascota?</h3>}
+                <button
+                type="button"
+                onClick={onClickChange}
+                >{disabled ? "Puedes decirme un estimado" : "Ya recuerdo cuando nacio" }</button>
+                {!disabled &&
+                    <div>
+                        {disabledAge ?
+                        <div>
+                            <h3>Años</h3>
+                            <div className="age-counter">
+                                <button
+                                type="button"
+                                onClick={() => setYears(years - 1)}
+                                disabled={years === 0}
+                                >-</button>
+                                <h1>{years}</h1>
+                                <button 
+                                type="button"
+                                onClick={() => setYears(years + 1)}
+                                >+</button>
+                            </div>
+                        </div>
+                        :
+                        <div>
+                            <h3>Meses</h3>
+                            <div className="age-counter">
+                                <button
+                                type="button"
+                                onClick={() => setMonths(months - 1)}
+                                disabled={months === 0}
+                                >-</button>
+                                <h1>{months}</h1>
+                                <button
+                                type="button"
+                                onClick={() => setMonths(months + 1)}
+                                disabled={months === 11}
+                                >+</button>
+                            </div>
+                        </div>
+                        }
+                        <button 
+                        type="button"
+                        onClick={onClickChangeAge}
+                        >
+                        {disabledAge ? '¿Tiene menos de un año?' : '¿Tiene mas de un año?'}
+                        </button>
+                    </div>
+                }
+                <select
+                onChange={formChange}
+                name="raza"
+                className={!form.raza && "danger"}
+                >
                     <option value="">Raza</option>
                     {razas.map(raza => (
                         <option key={raza.id} value={raza.nombre_raza}>{raza.nombre_raza}</option>
                     ))}
                 </select>
-                <h2>¿No sabes cuando nació exactamente tu mascota?</h2>
-                <h3>Puedes decirme un estimado</h3>
-                <input type="radio"/>Tiene mas de un año?
-                    <label for="Años">Años</label>
-                    <input type="Number"></input>
-                <input type="radio"/>Tiene menos de un año?
-                    <label for="Meses">Meses</label>
-                    <input type="Number"></input>
-                <select name="sexo" onChange={formChange}>
+                <select name="sexo" onChange={formChange} className={!form.sexo && "danger"}>
                     <option value="">Sexo</option>
                     <option value="Macho">Macho</option>
                     <option value="Hembra">Hembra</option>
                 </select>
-                <button type="submit">Submit</button>
+                <button 
+                type="submit"
+                disabled={!form.nombre || !form.raza || !form.sexo || (!form.edad && (months === 0 && years === 0 )) || (form.edad && (months !== 0 && years !== 0 ))}
+                >Enviar</button>
             </form>
         </div>
     );
